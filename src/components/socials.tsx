6e0d7f80406a4f93c52e98c3b3e1750d1173e7d3
varun325 <sharmavarun1300@gmail.com>
2024-04-14 20:34:03 +0530

    update social links

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -8,31 +8,43 @@ import {
 import { IconType } from "react-icons/lib";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { v4 as uuid } from "uuid";
-const socialButtonsAriaLabels: Map<IconType, string> = new Map([
-    [BsLinkedin, "LinkedIn"],
-    [BsMessenger, "Messenger"],
-    [BsWhatsapp, "Whatsapp"],
-    [BsTwitterX, "Twitter"],
-    [BsTelegram, "Telegram"],
-    [MdOutlineAlternateEmail, "Email"],
-]);
+const socialButtonsAriaLabels: Map<IconType, { title: string; url: string }> =
+    new Map([
+        [
+            BsLinkedin,
+            {
+                title: "LinkedIn",
+                url: "https://www.linkedin.com/in/varun-sharma325/",
+            },
+        ],
+        [BsMessenger, { title: "Messenger", url: "#" }],
+        [BsWhatsapp, { title: "Whatsapp", url: "#" }],
+        [BsTwitterX, { title: "Twitter", url: "#" }],
+        [BsTelegram, { title: "Telegram", url: "https://t.me/Iitaadorii" }],
+        [MdOutlineAlternateEmail, { title: "Email", url: "mailto:sharmavarun1300@gmail.com" }],
+    ]);
 export default function Socials() {
     return (
         <>
             {[
                 BsLinkedin,
-                BsMessenger,
-                BsWhatsapp,
+                // BsMessenger,
+                // BsWhatsapp,
                 BsTwitterX,
                 BsTelegram,
                 MdOutlineAlternateEmail,
             ].map((Icon) => (
-                <button
+                <a
+                    href={socialButtonsAriaLabels.get(Icon)?.url}
                     key={uuid()}
-                    aria-label={socialButtonsAriaLabels.get(Icon)}
+                    target="_blank"
                 >
-                    <Icon className="w-8 h-8 text-white" />
-                </button>
+                    <button
+                        aria-label={socialButtonsAriaLabels.get(Icon)?.title}
+                    >
+                        <Icon className="w-8 h-8 text-white" />
+                    </button>
+                </a>
             ))}
         </>
     );
