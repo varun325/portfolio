c7e6411bcf683f54a86b233f4ebbab520a2a52bd
varun325 <sharmavarun1300@gmail.com>
2024-04-15 18:19:17 +0530

    Make skills section responsive for mobile

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,13 +1,12 @@
-interface SkillInteface {
+interface SkillInterface {
     title: string;
     description: string;
     skills: string[];
 }
+
 export default function Skills({
     skillsData,
-}: Readonly<{
-    skillsData: SkillInteface[];
-}>) {
+}: Readonly<{ skillsData: SkillInterface[] }>) {
     const renderSkills = () => {
         const rows = [];
         let rowSize = 2; // Set the row size to 2 columns
@@ -15,13 +14,18 @@ export default function Skills({
         for (let i = 0; i < skillsData.length; i += rowSize) {
             const row = skillsData.slice(i, i + rowSize);
             rows.push(
-                <div key={`row-${i}`} className="flex flex-row space-x-4 mb-4">
+                <div
+                    key={`row-${i}`}
+                    className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 mb-4"
+                >
                     {row.map((skill) => (
                         <div
                             key={skill.title}
-                            className="w-1/2 border p-4 border border-4 border-[#525252]  rounded-2xl hover:shadow-md hover:shadow-[#525252]"
+                            className="w-full sm:w-1/2 border border-4 p-4 border-[#525252] rounded-2xl hover:shadow-md hover:shadow-[#525252]"
                         >
-                            <h1 className="break-words text-[#FEF9C3]">{skill.title}</h1>
+                            <h1 className="break-words text-[#FEF9C3]">
+                                {skill.title}
+                            </h1>
                             <p>{skill.description}</p>
                             <ul className="px-3 text-[#D6D3D1]">
                                 {skill.skills.map((s) => (
